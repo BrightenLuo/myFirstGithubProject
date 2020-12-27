ce1bb5f95d06455e16d4b75b1d32d78655625651
BrightenLuo <2636663928@qq.com>
2020-12-27 15:39:50 +0800

    添加循环，登录成功则退出，否则重新输入

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,29 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
+    /*
+    ctrl+alt+l可规范化代码
+     */
     public static void main(String[] args) {
         //File file=new File("D:\\IntelliJ IDEA 2019.2.4\\myFirstGithubProject\\src\\user.xlsx");//实际项目不是这样设置路径
         InputStream in=Main.class.getResourceAsStream("/user.xlsx");//读取本项目中的文件指定文件路径
         ReadExcel readExcel=new ReadExcel();
         User[] users=readExcel.readExcel(in);
-        System.out.println("请输入用户名：");
-        Scanner sc=new Scanner(System.in);
-        String username=sc.nextLine();
-        System.out.println("请输入密码:");
-        String password=sc.nextLine();
-        for(User user:users){
-            if(user.getUsername().equals(username)&&user.getPassword().equals(password)){
-                System.out.println("登陆成功");
-                return;
+        boolean bool=true;
+        while(bool) {
+            System.out.println("请输入用户名：");
+            Scanner sc = new Scanner(System.in);
+            String username = sc.nextLine();
+            System.out.println("请输入密码:");
+            String password = sc.nextLine();
+            for (User user : users) {
+                if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                    System.out.println("登陆成功");
+                    bool = false;
+                    break;
+                }
             }
+            if(bool) System.out.println("用户名或密码错误");
         }
-        System.out.println("用户名或密码错误");
     }
 }
