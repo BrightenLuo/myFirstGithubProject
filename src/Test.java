69313f7436fdf4f9e02e1d411a8e03e4773c637f
BrightenLuo <2636663928@qq.com>
2020-12-28 22:03:31 +0800

    优化添加购物车的代码，把购物车定义为和加购数量定义为类的静态变量。

--- src/Test.java
+++ src/Test.java
@@ -6,19 +6,24 @@ public class Test {
     ctrl+alt+l可规范化代码
      */
 
+    static Product[] carts = new Product[3];//购物车
+    static int count = 0;//购物车数量
+
+    //类的静态方法只能使用类的静态成员变量，而不能使用非静态成员变量
+
     public static void main(String[] args) {
         testUser();
         readProduction();
     }
 
     //验证用户名和密码
-    public static void testUser(){
+    public static void testUser() {
         //File file=new File("D:\\IntelliJ IDEA 2019.2.4\\myFirstGithubProject\\src\\user.xlsx");//实际项目不是这样设置路径
-        InputStream u= Test.class.getResourceAsStream("/user.xlsx");//读取本项目中的文件指定文件路径
-        ReadUser readExcel=new ReadUser();
-        User[] users=readExcel.readExcel(u);
-        boolean bool=true;
-        while(bool) {
+        InputStream u = Test.class.getResourceAsStream("/user.xlsx");//读取本项目中的文件指定文件路径
+        ReadUser readExcel = new ReadUser();
+        User[] users = readExcel.readExcel(u);
+        boolean bool = true;
+        while (bool) {
             System.out.println("请输入用户名：");
             Scanner sc = new Scanner(System.in);
             String username = sc.nextLine();
@@ -31,34 +36,32 @@ public class Test {
                     break;
                 }
             }
-            if(bool) System.out.println("用户名或密码错误");
+            if (bool) System.out.println("用户名或密码错误");
         }
     }
 
     //读取和显示商品
-    public static void readProduction(){
-        InputStream p= Test.class.getResourceAsStream("/product.xlsx");
-        ReadProduct readProduct=new ReadProduct();
-        Product[] products=readProduct.readExcel(p);
+    public static void readProduction() {
+        InputStream p = Test.class.getResourceAsStream("/product.xlsx");
+        ReadProduct readProduct = new ReadProduct();
+        Product[] products = readProduct.readExcel(p);
         System.out.println("可供购买的商品如下:");
-        for(Product pro:products) {
+        for (Product pro : products) {
             System.out.println(pro.toString());
         }
-        Product[] carts=new Product[3];//购物车
-        int count=0;//购物车数量
         System.out.println("请输入商品id，把该商品加入购物车：");
-        Scanner sc=new Scanner(System.in);
-        while(!sc.hasNext("exit")){
-            for(int i=0;i<products.length;i++){
-                if(sc.nextLine().equals(products[i].getId()))
-                    //carts[count++]=products[i];
-                    System.out.println(products[i].toString());
+        Scanner sc = new Scanner(System.in);
+        while (!sc.hasNext("exit")) {
+            String flag=sc.nextLine();
+            for (int i = 0; i < products.length; i++) {
+                if (flag.equals(products[i].getId()))
+                    carts[count++] = products[i];
             }
         }
         sc.close();
-        /*System.out.println("您的购物车商品如下:");
-        for(Product pr:carts){
-            System.out.println(pr.toString());
-        }*/
+        System.out.println("您的购物车商品如下:");
+        for (int i=0;i<count;i++) {
+            System.out.println(carts[i].toString());
+        }
     }
 }
